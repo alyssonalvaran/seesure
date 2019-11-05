f66731333dd49345ebfafd439016f8fa41eac92d
Alysson Alvaran <alvaran.alysson@gmail.com>
2019-11-05 20:23:01 +0800

    Remove dj_database_url

--- seesure/settings.py
+++ seesure/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -37,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'seesure',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -135,9 +136,4 @@ STATICFILES_DIRS = (
 #  Add configuration for static files storage using whitenoise
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
-import dj_database_url 
-prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
-
-import django_heroku
 django_heroku.settings(locals())
